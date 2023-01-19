ded9854368ced2be0a8ec03870b58ec69f55a4fc
Ruben Montes <rubenmontes330@gmail.com>
2023-01-19 18:32:42 +0000

    personalizar componentes con inputs

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,5 +1,5 @@
 
-import { Component, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { BaseChartDirective } from 'ng2-charts';
 import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
 
@@ -13,25 +13,46 @@ import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 })
 export class GraficaBarraComponent {
   @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  @Input() horizontal: boolean = false;
+  @Input() barChartData!: ChartData<'bar'>;
+  constructor(){}
+ 
+  ngOnInit(): void {
+      if (this.horizontal){
+          //Esto muestra la gráfica en sentido horizontal y todos los datos en ese sentido.
+          this.barChartOptions!.indexAxis = 'y';
+          this.barChartOptions!.scales!["y"]!.min = 0;
+      }
+  }
 
   public barChartOptions: ChartConfiguration['options'] = {
-    responsive: true,
-  
-  };
+      responsive: true,
+      scales: {
+          x: {},
+          y: {
+              min: 10
+          }
+      },
+      plugins: {
+          legend: {
+              display: true,
+          },
+          datalabels: {
+              anchor: 'end',
+              align: 'end'
+          }
+      }
+  }
+
   public barChartType: ChartType = 'bar';
   public barChartPlugins = [
-    DataLabelsPlugin
+      DataLabelsPlugin
   ];
+  
+  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {}
 
-  public barChartData: ChartData<'bar'> = {
-    labels: [ '2020', '2021', '2022', '2023', '2024', '2025', '2026' ],
-    datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#FFA442', hoverBackgroundColor:"#0B72B3" },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#702AFF',hoverBackgroundColor:"#0B72B3" },
-      { data: [ 38, 42, 23, 14, 47, 60, 70 ], label: 'Series C', backgroundColor: '#0FFF35', hoverBackgroundColor:"#0B72B3" }
+  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {}
 
-    ]
-  };
 
   
 
--- src/app/graficas/pages/barras-doble/barras-doble.component.ts
+++ src/app/graficas/pages/barras-doble/barras-doble.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-barras-doble',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   ]
 })
 export class BarrasDobleComponent {
-
+  labelsData: string[] = ['2021', '2022', '2023', '2024', '2025'];
+    
+    proveedoresData: ChartData<'bar'> = {
+        labels: this.labelsData,
+        datasets:[
+            { data: [100, 200, 300, 400, 500], label: 'Vendedor A' },
+            { data: [50, 250, 30, 450, 200], label: 'Vendedor B' }
+        ]
+    };
+ 
+    productoData: ChartData<'bar'> = {
+        labels: this.labelsData,
+        datasets:[
+            { data: [200, 300, 400, 300, 100], label: 'Carros', backgroundColor: 'blue' }
+        ]
+    };
+ 
 }
