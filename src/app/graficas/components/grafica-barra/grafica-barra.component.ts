8e23ea87a8e33046a7fc44ea325fbe0ce9a385d3
Ruben Montes <rubenmontes330@gmail.com>
2023-01-19 18:12:22 +0000

    Componente personalizado para mostrar multiples graficas

--- src/app/graficas/components/grafica-barra/grafica-barra.component.ts
+++ src/app/graficas/components/grafica-barra/grafica-barra.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
 
+import { Component, ViewChild } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+
+
+import DataLabelsPlugin from 'chartjs-plugin-datalabels';
 @Component({
   selector: 'app-grafica-barra',
   templateUrl: './grafica-barra.component.html',
@@ -7,5 +12,30 @@ import { Component } from '@angular/core';
   ]
 })
 export class GraficaBarraComponent {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  
+  };
+  public barChartType: ChartType = 'bar';
+  public barChartPlugins = [
+    DataLabelsPlugin
+  ];
+
+  public barChartData: ChartData<'bar'> = {
+    labels: [ '2020', '2021', '2022', '2023', '2024', '2025', '2026' ],
+    datasets: [
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A', backgroundColor: '#FFA442', hoverBackgroundColor:"#0B72B3" },
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B', backgroundColor: '#702AFF',hoverBackgroundColor:"#0B72B3" },
+      { data: [ 38, 42, 23, 14, 47, 60, 70 ], label: 'Series C', backgroundColor: '#0FFF35', hoverBackgroundColor:"#0B72B3" }
+
+    ]
+  };
+
+  
+
+  
 
 }
+ 
