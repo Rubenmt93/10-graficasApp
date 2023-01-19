97f0e98e290babdf63da612bbdc71687e8335ffc
Ruben Montes <rubenmontes330@gmail.com>
2023-01-19 17:37:54 +0000

    instalar  ng2-chart  y usar un componente de graficas

--- src/app/graficas/graficas.module.ts
+++ src/app/graficas/graficas.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgChartsModule } from 'ng2-charts';
+
+
 import { GraficasRoutingModule } from './graficas-routing.module';
 import { BarrasComponent } from './pages/barras/barras.component';
 import { BarrasDobleComponent } from './pages/barras-doble/barras-doble.component';
@@ -19,7 +22,9 @@ import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.
   ],
   imports: [
     CommonModule,
-    GraficasRoutingModule
+    GraficasRoutingModule,
+    NgChartsModule
+
   ]
 })
 export class GraficasModule { }
