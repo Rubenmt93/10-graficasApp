04eb3a4657a02735ce1fc5d01cafc835fa900de4
Ruben Montes <rubenmontes330@gmail.com>
2023-01-19 18:44:16 +0000

    Trabajo con graficas de donuts

--- src/app/graficas/pages/dona/dona.component.ts
+++ src/app/graficas/pages/dona/dona.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-dona',
@@ -7,5 +8,17 @@ import { Component } from '@angular/core';
   ]
 })
 export class DonaComponent {
+ // Doughnut
+ public doughnutChartLabels: string[] = [ 'Valor1', 'Valor2', 'Valor3', 'Otro' ];
+ public doughnutChartData: ChartData<'doughnut'> = {
+   labels: this.doughnutChartLabels,
+   datasets: [
+     { data: [ 350, 450, 100,150 ] ,backgroundColor: ['#6405F0','#0724E3', '#05A0F0', '#45FFB8'] },
+    
+   ]
+ };
+ public doughnutChartType: ChartType = 'doughnut';
+
 
 }
+ 
